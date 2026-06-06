26a14100d64d2e4ab7c4abfc04a4fbddabe0a3af
mrabukar <abukarmacalin08@gmail.com>
2026-06-06 16:51:05 +0300

    Add products CRUD endpoints.

---
+++ api/src/modules/products/products.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common";
+import { UserRole } from "@prisma/client";
+import {
+  CurrentUser,
+  type CurrentUserPayload,
+} from "../../common/decorators/current-user.decorator";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { ProductQueryDto } from "./dto/product-query.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
+import { ProductsService } from "./products.service";
+
+@Roles(UserRole.admin)
+@Controller("products")
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll(@Query() query: ProductQueryDto) {
+    return this.productsService.findAll(query);
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(
+    @Body() dto: CreateProductDto,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    return this.productsService.create(dto, user);
+  }
+
+  @Patch(":id/deactivate")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deactivate(@Param("id") id: string, @CurrentUser() user: CurrentUserPayload) {
+    return this.productsService.deactivate(id, user);
+  }
+
+  @Patch(":id/reactivate")
+  reactivate(@Param("id") id: string, @CurrentUser() user: CurrentUserPayload) {
+    return this.productsService.reactivate(id, user);
+  }
+
+  @Patch(":id")
+  update(
+    @Param("id") id: string,
+    @Body() dto: UpdateProductDto,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    return this.productsService.update(id, dto, user);
+  }
+}
