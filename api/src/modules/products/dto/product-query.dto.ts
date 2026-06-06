32bd0333f55158bc3a5cf6d9e5c7359722a7bb20
mrabukar <abukarmacalin08@gmail.com>
2026-06-06 16:51:05 +0300

    Add product list query DTO.

---
+++ api/src/modules/products/dto/product-query.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from "class-validator";
+import { Type } from "class-transformer";
+import { PaginationQueryDto } from "../../../common/dto/pagination-query.dto";
+
+export class ProductQueryDto extends PaginationQueryDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  search?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  categoryId?: number;
+}
