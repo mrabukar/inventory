351f7fbaf452c0d525e1fefd58b0d482d8ba657e
mrabukar <abukarmacalin08@gmail.com>
2026-06-06 16:51:06 +0300

    Add inventory list query DTO.

---
+++ api/src/modules/inventory/dto/inventory-query.dto.ts
@@ -0,0 +1,30 @@
+import { Transform, Type } from "class-transformer";
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from "class-validator";
+import { PaginationQueryDto } from "../../../common/dto/pagination-query.dto";
+
+export class InventoryQueryDto extends PaginationQueryDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  search?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  categoryId?: number;
+
+  @IsOptional()
+  @Transform(
+    ({ value }: { value: unknown }) => value === true || value === "true",
+  )
+  @IsBoolean()
+  lowStockOnly?: boolean;
+}
