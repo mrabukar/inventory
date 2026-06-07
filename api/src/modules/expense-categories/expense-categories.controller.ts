de584b35b6ae655d7aecfc941193c5961db57356
mrabukar <abukarmacalin08@gmail.com>
2026-06-07 14:38:58 +0300

    Add expense categories REST controller.

---
+++ api/src/modules/expense-categories/expense-categories.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from "@nestjs/common";
+import { UserRole } from "@prisma/client";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { CreateExpenseCategoryDto } from "./dto/create-expense-category.dto";
+import { UpdateExpenseCategoryDto } from "./dto/update-expense-category.dto";
+import { ExpenseCategoriesService } from "./expense-categories.service";
+
+@Roles(UserRole.admin)
+@Controller("expense-categories")
+export class ExpenseCategoriesController {
+  constructor(
+    private readonly expenseCategoriesService: ExpenseCategoriesService,
+  ) {}
+
+  @Get()
+  findAll() {
+    return this.expenseCategoriesService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.expenseCategoriesService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() dto: CreateExpenseCategoryDto) {
+    return this.expenseCategoriesService.create(dto);
+  }
+
+  @Patch(":id")
+  update(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() dto: UpdateExpenseCategoryDto,
+  ) {
+    return this.expenseCategoriesService.update(id, dto);
+  }
+
+  @Delete(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.expenseCategoriesService.remove(id);
+  }
+}
