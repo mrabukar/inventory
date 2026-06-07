01a4ed9f45867989fb737f947b7fbca10a85b36e
mrabukar <abukarmacalin08@gmail.com>
2026-06-07 14:39:07 +0300

    Add reports REST controller.

---
+++ api/src/modules/reports/reports.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Query } from "@nestjs/common";
+import { UserRole } from "@prisma/client";
+import {
+  CurrentUser,
+  type CurrentUserPayload,
+} from "../../common/decorators/current-user.decorator";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { ReportQueryDto } from "./dto/report-query.dto";
+import { ReportsService } from "./reports.service";
+
+@Controller("reports")
+export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
+  @Get("admin-dashboard")
+  @Roles(UserRole.admin)
+  getAdminDashboard(@Query() query: ReportQueryDto) {
+    return this.reportsService.getAdminDashboard(query);
+  }
+
+  @Get("manager-dashboard")
+  @Roles(UserRole.branch_manager)
+  getManagerDashboard(@CurrentUser() user: CurrentUserPayload) {
+    return this.reportsService.getManagerDashboard(user);
+  }
+
+  @Get("financial-summary")
+  @Roles(UserRole.admin)
+  getFinancialSummary(@Query() query: ReportQueryDto) {
+    return this.reportsService.getFinancialSummary(query);
+  }
+}
