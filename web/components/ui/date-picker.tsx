6f92a52ac264e19a7142e916569a02c95bb2d1bf
mrabukar <abukarmacalin08@gmail.com>
2026-06-11 15:32:37 +0300

    Improve date picker for expense and report filters.

--- web/components/ui/date-picker.tsx
+++ web/components/ui/date-picker.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { cn } from "@/lib/utils"
-import { Calendar } from "@/components/calendar"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import * as React from "react"
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import * as React from "react";
 
 export default function DatePicker() {
-  const [date, setDate] = React.useState<Date>()
+  const [date, setDate] = React.useState<Date>();
 
   return (
     <Popover>
@@ -22,7 +22,7 @@ export default function DatePicker() {
           variant="outline"
           className={cn(
             "w-[240px] justify-start text-left font-normal",
-            !date && "text-muted-foreground"
+            !date && "text-muted-foreground",
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
@@ -33,5 +33,5 @@ export default function DatePicker() {
         <Calendar mode="single" selected={date} onSelect={setDate} autoFocus />
       </PopoverContent>
     </Popover>
-  )
+  );
 }
