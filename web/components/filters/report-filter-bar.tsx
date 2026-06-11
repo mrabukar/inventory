8669b91a41be44b28858fb7638ddc1256e8029ef
mrabukar <abukarmacalin08@gmail.com>
2026-06-11 22:31:35 +0300

    Support optional category filter in report filter bar.

--- web/components/filters/report-filter-bar.tsx
+++ web/components/filters/report-filter-bar.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import type { ReportFilters } from "@/hooks/filters/use-report-filters";
+import { CategoryFilter } from "./category-filter";
 import { DateRangeFilter } from "./date-range-filter";
 import { StoreFilter } from "./store-filter";
 
 interface ReportFilterBarProps {
   filters: ReportFilters;
+  showCategoryFilter?: boolean;
 }
 
-export function ReportFilterBar({ filters }: ReportFilterBarProps) {
+export function ReportFilterBar({
+  filters,
+  showCategoryFilter = false,
+}: ReportFilterBarProps) {
   return (
     <div style={{ display: "flex", gap: 10 }}>
       <DateRangeFilter filters={filters} />
       <StoreFilter filters={filters} />
+      {showCategoryFilter ? <CategoryFilter filters={filters} /> : null}
     </div>
   );
 }
