5ad3e856334ff42b394f4bf0a4a8768f237f8b3a
mrabukar <abukarmacalin08@gmail.com>
2026-06-13 16:14:08 +0300

    Delegate /me endpoint to MeService for store-enriched profile.

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 import { Session } from "@thallesp/nestjs-better-auth";
+import { MeService } from "./me.service";
 
 @Controller()
 export class MeController {
+  constructor(private readonly meService: MeService) {}
+
   @Get("me")
-  me(@Session() session: { user: unknown }) {
-    return { user: session.user };
+  me(@Session() session: { user: Record<string, unknown> }) {
+    return this.meService.getProfile(session.user);
   }
 }
