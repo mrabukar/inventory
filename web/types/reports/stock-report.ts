b47eb469222685979553a771a259c641cbfc6118
mrabukar <abukarmacalin08@gmail.com>
2026-06-13 14:17:01 +0300

    Add TypeScript types for the stock report API response.

---
+++ web/types/reports/stock-report.ts
@@ -0,0 +1,30 @@
+import type { ReportPeriod } from "./common";
+import type { ReportQuery } from "./query";
+
+export interface StockReportProduct {
+  productId: string;
+  productName: string;
+  purchaseDevices: number;
+  inStock: number;
+  salesDevices: number;
+}
+
+export interface StockReportTotals {
+  purchaseDevices: number;
+  inStock: number;
+  salesDevices: number;
+}
+
+export interface StockReportFilters {
+  storeId: string | null;
+  categoryId: number | null;
+}
+
+export interface StockReportResponse {
+  period: ReportPeriod;
+  filters: StockReportFilters;
+  totals: StockReportTotals;
+  products: StockReportProduct[];
+}
+
+export type StockReportQuery = ReportQuery;
