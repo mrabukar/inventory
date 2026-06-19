7fec29fc97cffd342977a893cc45d8e54b967d13
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 12:16:52 +0300

    Export tenant store resolver from stores module.

--- api/src/modules/stores/stores.module.ts
+++ api/src/modules/stores/stores.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { TenantStoreResolver } from "../../common/tenant/tenant-store-resolver.service";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { StoresController } from "./stores.controller";
 import { StoresService } from "./stores.service";
@@ -6,7 +7,7 @@ import { StoresService } from "./stores.service";
 @Module({
   imports: [PrismaModule],
   controllers: [StoresController],
-  providers: [StoresService],
-  exports: [StoresService],
+  providers: [StoresService, TenantStoreResolver],
+  exports: [StoresService, TenantStoreResolver],
 })
 export class StoresModule {}
