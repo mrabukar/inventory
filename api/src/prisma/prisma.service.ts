6b80bb195dd97074dc452557443cc5a7fbdec1d5
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 12:16:38 +0300

    Apply tenant scoping extension to Prisma client.

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import { TenantContextService } from "../common/tenant/tenant-context.service";
+import { createTenantExtension } from "./tenant-scoping.extension";
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(private readonly tenantContext: TenantContextService) {
+    super();
+    const extended = this.$extends(createTenantExtension(tenantContext));
+    return extended as unknown as PrismaService;
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
