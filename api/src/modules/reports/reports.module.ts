59620c699d99b7bd3b21dc23fba4b384edee9273
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 12:16:56 +0300

    Import stores module for tenant-aware stock reports.

--- api/src/modules/reports/reports.module.ts
+++ api/src/modules/reports/reports.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
+import { StoresModule } from "../stores/stores.module";
 import { ReportsController } from "./reports.controller";
 import { ReportsService } from "./reports.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StoresModule],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
