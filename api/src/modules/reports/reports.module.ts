5007559d31860b01e324900ec281233ef46a1654
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 23:36:45 +0300

    Wire report export service and storage into reports module.

--- api/src/modules/reports/reports.module.ts
+++ api/src/modules/reports/reports.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { StoresModule } from "../stores/stores.module";
+import { ReportExportService } from "./export/report-export.service";
 import { ReportsController } from "./reports.controller";
 import { ReportsService } from "./reports.service";
 
 @Module({
   imports: [PrismaModule, StoresModule],
   controllers: [ReportsController],
-  providers: [ReportsService],
+  providers: [ReportsService, ReportExportService],
 })
 export class ReportsModule {}
