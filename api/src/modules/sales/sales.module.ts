6244be5de20cb16e97237d2677514bad62b8ab3a
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 12:16:55 +0300

    Import stores module for tenant store resolution in sales.

--- api/src/modules/sales/sales.module.ts
+++ api/src/modules/sales/sales.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { ProductsModule } from "../products/products.module";
+import { StoresModule } from "../stores/stores.module";
 import { SalesController } from "./sales.controller";
 import { SalesService } from "./sales.service";
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, StoresModule],
   controllers: [SalesController],
   providers: [SalesService],
   exports: [SalesService],
