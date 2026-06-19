14dc2f27f315b292c8603a59bf4d99f383287240
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 12:16:54 +0300

    Make storeId optional for direct-sales stock supply.

--- api/src/modules/stock-supplies/dto/create-stock-supply.dto.ts
+++ api/src/modules/stock-supplies/dto/create-stock-supply.dto.ts
@@ -14,9 +14,10 @@ export class CreateStockSupplyDto {
   @IsNotEmpty()
   productId!: string;
 
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
-  storeId!: string;
+  storeId?: string;
 
   @Type(() => Number)
   @IsInt()
