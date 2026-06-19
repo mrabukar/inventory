6d3beece15171989e2d2bf3a93360cc03653bb40
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 18:51:35 +0300

    Wire organization logo controller into organizations module.

--- api/src/modules/organizations/organizations.module.ts
+++ api/src/modules/organizations/organizations.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { StoresModule } from "../stores/stores.module";
 import { OrganizationsController } from "./organizations.controller";
+import { OrganizationsLogoController } from "./organizations-logo.controller";
 import { OrganizationsService } from "./organizations.service";
 
 @Module({
   imports: [PrismaModule, StoresModule],
-  controllers: [OrganizationsController],
+  controllers: [OrganizationsController, OrganizationsLogoController],
   providers: [OrganizationsService],
 })
 export class OrganizationsModule {}
