d62a97bd6dcdfc273a84a439e4ae1b155ae36c9a
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 23:36:46 +0300

    Pass disabled state through date range filter.

--- web/components/filters/date-range-filter.tsx
+++ web/components/filters/date-range-filter.tsx
@@ -5,13 +5,15 @@ import { DateRangePicker } from "@/components/ui/date-range-picker";
 
 interface DateRangeFilterProps {
   filters: ReportFilters;
+  disabled?: boolean;
 }
 
-export function DateRangeFilter({ filters }: DateRangeFilterProps) {
+export function DateRangeFilter({ filters, disabled = false }: DateRangeFilterProps) {
   return (
     <DateRangePicker
       fromDate={filters.query.fromDate}
       toDate={filters.query.toDate}
+      disabled={disabled}
       onChange={(range) =>
         filters.setDateRange(range.fromDate, range.toDate)
       }
