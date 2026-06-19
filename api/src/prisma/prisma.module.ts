c0599c380136a26dfbf863ef06eb4501d7df0ba2
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 12:16:39 +0300

    Wire tenant context into Prisma module.

--- api/src/prisma/prisma.module.ts
+++ api/src/prisma/prisma.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from "@nestjs/common";
+import { TenantModule } from "../common/tenant/tenant.module";
 import { PrismaService } from "./prisma.service";
 
 @Global()
 @Module({
+  imports: [TenantModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
