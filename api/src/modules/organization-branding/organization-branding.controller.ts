35372af67b774eaa28c207de0e5eea4aa16e1ca4
mrabukar <abukarmacalin08@gmail.com>
2026-06-19 18:51:19 +0300

    Add org admin logo upload, download, and delete endpoints.

---
+++ api/src/modules/organization-branding/organization-branding.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { UserRole } from "@prisma/client";
+import type { Response } from "express";
+import { memoryStorage } from "multer";
+import {
+  CurrentUser,
+  type CurrentUserPayload,
+} from "../../common/decorators/current-user.decorator";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { requireOrganizationId } from "../../common/utils/require-organization-id.util";
+import { OrganizationLogoService } from "../../common/storage/organization-logo.service";
+import { ORGANIZATION_LOGO_MAX_BYTES } from "../../common/storage/organization-logo.constants";
+
+@Roles(UserRole.admin)
+@Controller("organization")
+export class OrganizationBrandingController {
+  constructor(private readonly organizationLogo: OrganizationLogoService) {}
+
+  @Post("logo")
+  @UseInterceptors(
+    FileInterceptor("logo", {
+      storage: memoryStorage(),
+      limits: { fileSize: ORGANIZATION_LOGO_MAX_BYTES },
+    }),
+  )
+  uploadLogo(
+    @UploadedFile() file: Express.Multer.File,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    const organizationId = requireOrganizationId(user);
+    return this.organizationLogo.uploadLogo(organizationId, user, file);
+  }
+
+  @Delete("logo")
+  deleteLogo(@CurrentUser() user: CurrentUserPayload) {
+    const organizationId = requireOrganizationId(user);
+    return this.organizationLogo.deleteLogo(organizationId, user);
+  }
+
+  @Get("logo")
+  async getLogo(
+    @CurrentUser() user: CurrentUserPayload,
+    @Res() res: Response,
+  ) {
+    const organizationId = requireOrganizationId(user);
+    const object = await this.organizationLogo.getLogoObject(
+      organizationId,
+      user,
+    );
+    res.setHeader("Content-Type", object.contentType);
+    res.setHeader("Cache-Control", "private, max-age=300");
+    res.send(object.body);
+  }
+}
