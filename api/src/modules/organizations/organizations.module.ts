d105a6775e5631a31a16c2b1b1569c64c865df7b
mrabukar <abukarmacalin08@gmail.com>
2026-06-20 07:09:55 +0000

    Import UsersModule for organization user management.

--- api/src/modules/organizations/organizations.module.ts
+++ api/src/modules/organizations/organizations.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
+import { UsersModule } from "../users/users.module";
 import { StoresModule } from "../stores/stores.module";
 import { OrganizationsController } from "./organizations.controller";
 import { OrganizationsLogoController } from "./organizations-logo.controller";
 import { OrganizationsService } from "./organizations.service";
 
 @Module({
-  imports: [PrismaModule, StoresModule],
+  imports: [PrismaModule, StoresModule, UsersModule],
   controllers: [OrganizationsController, OrganizationsLogoController],
   providers: [OrganizationsService],
   exports: [OrganizationsService],
