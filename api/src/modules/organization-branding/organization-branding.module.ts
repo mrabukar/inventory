4ba894ff56bfc43499f994c3fe1b8a918f341663
mrabukar <abukarmacalin08@gmail.com>
2026-06-20 06:43:03 +0300

    Import OrganizationsModule for current organization endpoints.

--- api/src/modules/organization-branding/organization-branding.module.ts
+++ api/src/modules/organization-branding/organization-branding.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { OrganizationsModule } from "../organizations/organizations.module";
 import { OrganizationBrandingController } from "./organization-branding.controller";
 
 @Module({
+  imports: [OrganizationsModule],
   controllers: [OrganizationBrandingController],
 })
 export class OrganizationBrandingModule {}
