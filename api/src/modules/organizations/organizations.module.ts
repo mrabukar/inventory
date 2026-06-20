6559b51ccb7e9fddb969fdf5420a8187a142e5d0
mrabukar <abukarmacalin08@gmail.com>
2026-06-20 06:43:04 +0300

    Export OrganizationsService for organization branding module.

--- api/src/modules/organizations/organizations.module.ts
+++ api/src/modules/organizations/organizations.module.ts
@@ -9,5 +9,6 @@ import { OrganizationsService } from "./organizations.service";
   imports: [PrismaModule, StoresModule],
   controllers: [OrganizationsController, OrganizationsLogoController],
   providers: [OrganizationsService],
+  exports: [OrganizationsService],
 })
 export class OrganizationsModule {}
