b139bf948b086ce8c86bcb0db0444f134dc32d71
mrabukar <abukarmacalin08@gmail.com>
2026-06-21 16:33:37 +0300

    Add positive selling price validation helper.

--- api/src/modules/products/product-price.util.ts
+++ api/src/modules/products/product-price.util.ts
@@ -1,5 +1,11 @@
 import { BadRequestException } from "@nestjs/common";
 
+export function assertPositiveSellingPrice(sellingPrice: number): void {
+  if (sellingPrice <= 0) {
+    throw new BadRequestException("Selling price must be greater than zero");
+  }
+}
+
 export function assertSellingPriceNotBelowPurchase(
   purchasePrice: number,
   sellingPrice: number,
