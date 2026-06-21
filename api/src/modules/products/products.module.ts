07be1c90e826de967087d8b9b7312ec6d4f3d287
mrabukar <abukarmacalin08@gmail.com>
2026-06-21 16:33:37 +0300

    Wire PurchasesModule into ProductsModule.

--- api/src/modules/products/products.module.ts
+++ api/src/modules/products/products.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
+import { PurchasesModule } from "../purchases/purchases.module";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(() => PurchasesModule)],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
