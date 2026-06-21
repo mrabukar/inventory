2a76d840a4ece5e3cc4f9867fd39b6dbc9851db6
mrabukar <abukarmacalin08@gmail.com>
2026-06-21 16:33:42 +0300

    Add purchases module for procurement and weighted-average cost.

---
+++ api/src/modules/purchases/dto/purchase-query.dto.ts
@@ -0,0 +1,39 @@
+import { Type } from "class-transformer";
+import {
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from "class-validator";
+import { PaginationQueryDto } from "../../../common/dto/pagination-query.dto";
+
+export class PurchaseQueryDto extends PaginationQueryDto {
+  @IsOptional()
+  @IsString()
+  productId?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  invoiceNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  fromDate?: string;
+
+  @IsOptional()
+  @IsString()
+  toDate?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  categoryId?: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  search?: string;
+}
