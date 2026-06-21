908593e8f6b11833b92938462c71fbcb677f2d4d
mrabukar <abukarmacalin08@gmail.com>
2026-06-21 16:33:42 +0300

    Add purchases module for procurement and weighted-average cost.

---
+++ api/src/modules/purchases/purchases.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from "@nestjs/common";
+import { UserRole } from "@prisma/client";
+import {
+  CurrentUser,
+  type CurrentUserPayload,
+} from "../../common/decorators/current-user.decorator";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { CreatePurchaseDto } from "./dto/create-purchase.dto";
+import { PurchaseQueryDto } from "./dto/purchase-query.dto";
+import { PurchasesService } from "./purchases.service";
+
+@Roles(UserRole.admin)
+@Controller("purchases")
+export class PurchasesController {
+  constructor(private readonly purchasesService: PurchasesService) {}
+
+  @Get()
+  findAll(@Query() query: PurchaseQueryDto) {
+    return this.purchasesService.findAll(query);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(
+    @Body() dto: CreatePurchaseDto,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    return this.purchasesService.create(dto, user);
+  }
+}
