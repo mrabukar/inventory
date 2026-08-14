2f91e00a8ed71d3c73f70799f9d3041155ccd606
mrabukar <abukarmacalin08@gmail.com>
2026-08-14 11:49:09 +0300

    Add create-customer.dto.ts

---
+++ api/src/modules/customers/dto/create-customer.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from "class-validator";
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(150)
+  name!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(30)
+  phone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(150)
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(250)
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  note?: string;
+}
