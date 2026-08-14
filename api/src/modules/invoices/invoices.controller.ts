3746aedb17d8e24f0787fba676628cdf7597cd9f
mrabukar <abukarmacalin08@gmail.com>
2026-08-14 11:49:10 +0300

    Add invoices.controller.ts

---
+++ api/src/modules/invoices/invoices.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param, Query } from "@nestjs/common";
+import { UserRole } from "@prisma/client";
+import {
+  CurrentUser,
+  type CurrentUserPayload,
+} from "../../common/decorators/current-user.decorator";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { InvoiceQueryDto } from "./dto/invoice-query.dto";
+import { InvoicesService } from "./invoices.service";
+
+@Roles(UserRole.admin)
+@Controller("invoices")
+export class InvoicesController {
+  constructor(private readonly invoicesService: InvoicesService) {}
+
+  @Get()
+  findAll(
+    @Query() query: InvoiceQueryDto,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    return this.invoicesService.findAll(query, user);
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string, @CurrentUser() user: CurrentUserPayload) {
+    return this.invoicesService.findOne(id, user);
+  }
+}
