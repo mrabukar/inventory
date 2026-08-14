dd434699ce5e5e519c1cd448d9f679fcb600d0ed
mrabukar <abukarmacalin08@gmail.com>
2026-08-14 11:49:13 +0300

    Add payment-query.dto.ts

---
+++ api/src/modules/payments/dto/payment-query.dto.ts
@@ -0,0 +1,12 @@
+import { IsOptional, IsString } from "class-validator";
+import { PaginationQueryDto } from "../../../common/dto/pagination-query.dto";
+
+export class PaymentQueryDto extends PaginationQueryDto {
+  @IsOptional()
+  @IsString()
+  customerId?: string;
+
+  @IsOptional()
+  @IsString()
+  invoiceId?: string;
+}
