0589bdfe59259074d3ecf0b3eedae831bb15b96e
mrabukar <abukarmacalin08@gmail.com>
2026-08-14 11:49:11 +0300

    Update update-current-organization.dto.ts

--- api/src/modules/organization-branding/dto/update-current-organization.dto.ts
+++ api/src/modules/organization-branding/dto/update-current-organization.dto.ts
@@ -1,8 +1,23 @@
-import { IsOptional, IsString, MinLength } from "class-validator";
+import { IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UpdateCurrentOrganizationDto {
   @IsOptional()
   @IsString()
   @MinLength(1)
   name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(30)
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
+  paymentNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(250)
+  address?: string;
 }
