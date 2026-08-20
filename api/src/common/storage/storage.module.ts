3ca94ce93d88d7109b19c94da634823a0755a8e9
mrabukar <abukarmacalin08@gmail.com>
2026-08-20 12:52:20 +0300

    Register OrganizationSignatureService in StorageModule

--- api/src/common/storage/storage.module.ts
+++ api/src/common/storage/storage.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from "@nestjs/common";
 import { ObjectStorageService } from "./object-storage.service";
 import { OrganizationLogoService } from "./organization-logo.service";
+import { OrganizationSignatureService } from "./organization-signature.service";
 import { OrganizationStampService } from "./organization-stamp.service";
 
 @Global()
@@ -9,11 +10,13 @@ import { OrganizationStampService } from "./organization-stamp.service";
     ObjectStorageService,
     OrganizationLogoService,
     OrganizationStampService,
+    OrganizationSignatureService,
   ],
   exports: [
     ObjectStorageService,
     OrganizationLogoService,
     OrganizationStampService,
+    OrganizationSignatureService,
   ],
 })
 export class StorageModule {}
