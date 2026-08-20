edc4c62ffc1d833a5e4d9949b2495285549c224d
mrabukar <abukarmacalin08@gmail.com>
2026-08-20 12:52:21 +0300

    Add organization signature upload and fetch routes

--- api/src/modules/organization-branding/organization-branding.controller.ts
+++ api/src/modules/organization-branding/organization-branding.controller.ts
@@ -23,6 +23,8 @@ import { OrganizationLogoService } from "../../common/storage/organization-logo.
 import { ORGANIZATION_LOGO_MAX_BYTES } from "../../common/storage/organization-logo.constants";
 import { OrganizationStampService } from "../../common/storage/organization-stamp.service";
 import { ORGANIZATION_STAMP_MAX_BYTES } from "../../common/storage/organization-stamp.constants";
+import { OrganizationSignatureService } from "../../common/storage/organization-signature.service";
+import { ORGANIZATION_SIGNATURE_MAX_BYTES } from "../../common/storage/organization-signature.constants";
 import { OrganizationsService } from "../organizations/organizations.service";
 import { UpdateCurrentOrganizationDto } from "./dto/update-current-organization.dto";
 
@@ -32,6 +34,7 @@ export class OrganizationBrandingController {
   constructor(
     private readonly organizationLogo: OrganizationLogoService,
     private readonly organizationStamp: OrganizationStampService,
+    private readonly organizationSignature: OrganizationSignatureService,
     private readonly organizationsService: OrganizationsService,
   ) {}
 
@@ -109,6 +112,46 @@ export class OrganizationBrandingController {
   ) {
     const organizationId = requireOrganizationId(user);
     const object = await this.organizationStamp.getStampObject(
+      organizationId,
+      user,
+    );
+    res.setHeader("Content-Type", object.contentType);
+    res.setHeader("Cache-Control", "private, max-age=300");
+    res.send(object.body);
+  }
+
+  @Post("signature")
+  @UseInterceptors(
+    FileInterceptor("signature", {
+      storage: memoryStorage(),
+      limits: { fileSize: ORGANIZATION_SIGNATURE_MAX_BYTES },
+    }),
+  )
+  uploadSignature(
+    @UploadedFile() file: Express.Multer.File,
+    @CurrentUser() user: CurrentUserPayload,
+  ) {
+    const organizationId = requireOrganizationId(user);
+    return this.organizationSignature.uploadSignature(
+      organizationId,
+      user,
+      file,
+    );
+  }
+
+  @Delete("signature")
+  deleteSignature(@CurrentUser() user: CurrentUserPayload) {
+    const organizationId = requireOrganizationId(user);
+    return this.organizationSignature.deleteSignature(organizationId, user);
+  }
+
+  @Get("signature")
+  async getSignature(
+    @CurrentUser() user: CurrentUserPayload,
+    @Res() res: Response,
+  ) {
+    const organizationId = requireOrganizationId(user);
+    const object = await this.organizationSignature.getSignatureObject(
       organizationId,
       user,
     );
